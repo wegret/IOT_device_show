11f4b955fd129119409d9c66b1b89bad7fc9761e
wegret <wegret@163.com>
2024-08-12 21:00:08 +0800

    Update.

--- vite.config.ts
+++ vite.config.ts
@@ -5,33 +5,36 @@ import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => ['v-list-recognize-title'].includes(tag)
+    plugins: [
+        vue({
+            template: {
+                compilerOptions: {
+                    isCustomElement: (tag) => ['v-list-recognize-title'].includes(tag)
+                }
+            }
+        }),
+        vuetify({
+            autoImport: true
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         }
-      }
-    }),
-    vuetify({
-      autoImport: true
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {}
-    }
-  },
-  build: {
-    chunkSizeWarningLimit: 1024 * 1024 // Set the limit to 1 MB
-  },
-  optimizeDeps: {
-    exclude: ['vuetify'],
-    entries: ['./src/**/*.vue']
-  }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {}
+        }
+    },
+    build: {
+        chunkSizeWarningLimit: 1024 * 1024 // Set the limit to 1 MB
+    },
+    optimizeDeps: {
+        exclude: ['vuetify'],
+        entries: ['./src/**/*.vue']
+    },
+    server: {
+        host: '0.0.0.0'
+    },
 });
